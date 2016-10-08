65fe6737518bb42a85c2b096ec054729b0ad0cf1
Newcon <583641974@qq.com>
2016-10-08 02:44:57 -0700

    10.8 first modify

---
+++ learnSecond/learnSecond/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  learnSecond
+//
+//  Created by Newcon on 2016/10/8.
+//  Copyright © 2016年 Newcon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"yiyiyiiyyi");
+    }
+    return 0;
+}
